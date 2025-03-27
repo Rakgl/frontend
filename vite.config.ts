a1fdaa81dc25a184ee8d4b9dffbf1eaf937782c1
Rakgl <rakkosal7@gmail.com>
2025-03-27 23:45:50 +0700

    new update the code

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
+import qrcode from 'qrcode-terminal';
+
+const DEPLOYED_URL = 'https://frontend-rak.vercel.app/';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-
+    {
+      name: 'vite-plugin-qrcode',
+      configResolved() {
+        qrcode.generate(DEPLOYED_URL, { small: true }, (qr) => {
+          console.log(qr);
+        });
+      },
+    },
   ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
 })
